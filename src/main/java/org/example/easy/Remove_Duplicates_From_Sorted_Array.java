6b7d72d3946a179044fad4b5c839602345b581c7
Arda <ardakoksals@icloud.com>
2024-11-10 16:03:32 +0100

    update

---
+++ src/main/java/org/example/easy/Remove_Duplicates_From_Sorted_Array.java
@@ -0,0 +1,18 @@
+package org.example.easy;
+
+public class Remove_Duplicates_From_Sorted_Array {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
+
